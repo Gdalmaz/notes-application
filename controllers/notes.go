333e2951b923e454f8303e0f67cce3a0918647af
Gdalmaz <gokhandalmaz@hotmail.com>
2024-07-04 12:42:01 +0300

    KEY-1

---
+++ controllers/notes.go
@@ -0,0 +1,19 @@
+package controllers
+
+import "github.com/gofiber/fiber/v2"
+
+func CreateNotes(c *fiber.Ctx) error {
+	return nil
+}
+
+func UpdateNotes(c *fiber.Ctx) error {
+	return nil
+}
+
+func DeleteNotes(c *fiber.Ctx) error {
+	return nil
+}
+
+func GetAllNotes(c *fiber.Ctx) error {
+	return nil
+}
